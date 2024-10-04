6bf630c6e41184d6d6b07dfe012252b5fc894692
D4n0_CZ <145021685+DanoHrCZ@users.noreply.github.com>
2024-10-04 15:22:15 +0200

    landing

---
+++ client/src/modules/Abou.jsx
@@ -0,0 +1,11 @@
+import "../styles/modules.css"
+
+const About = () =>{
+    return<>
+    <div className="about-module block radius">
+        <h2>O nás</h2>
+        <p>Jsme překladatelská skupina <b>HajimariSubs</b>, zaměřující se na překlady anime. Naše skupina se skládá z vášnivých překladatelů s hlubokým porozuměním jazykům a kulturám. Snažíme se přinášet českým fanouškům kvalitní a přesné titulky, které jim umožní plně si užívat jejich oblíbené anime seriály a filmy.Naše práce je založena na maximální věrnosti originálním dialogům, přičemž zároveň dbáme na srozumitelnost a přístupnost překladů. Každý překlad pečlivě prochází kontrolou a editací, abychom zajistili nejvyšší možnou kvalitu. Naše skupina se neustále rozvíjí a rozšiřuje své portfolio, aby co nejlépe vyhověla požadavkům naší komunity.Kromě překládání anime rádi komunikujeme s našimi fanoušky a vítáme jejich zpětnou vazbu. Vaše podněty a návrhy jsou pro nás cenné a pomáhají nám v neustálém zlepšování našich služeb. Pro kontaktování nás můžete použít e-mailovou adresu:</p>
+    </div>
+    </>
+}
+export default About;
--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,5 +1,9 @@
+// modlue imports
+import About from "../modules/Abou"
+
 const Landing = () => {
     return<>
+    <About></About>
     </>
 }
 
